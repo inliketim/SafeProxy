27b40c56d6958b12d2e999849e12355e862674c5
inliketim <inliketim@users.noreply.github.com>
2015-05-04 21:44:34 -0700

    Added safeParameters method and corresponding test. Test framework files (qUnit and Sinon) now included in repo. Added MIT license text to source javascript.

--- SafeProxy.js
+++ SafeProxy.js
@@ -1,4 +1,30 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Tim Rozycki
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 SafeProxy = {};
+
 SafeProxy.safe = function(funcToProxy){
 	var result = function(){
 		try{
@@ -10,6 +36,25 @@ SafeProxy.safe = function(funcToProxy){
 			};
 			setTimeout(handler, 1);
 		};
-	}
+	};
 	return result;
 };
+
+SafeProxy.safeParameters = function(funcToProxy){
+	var result = function(){
+		safeArguments = [];
+		if(arguments && arguments.length){
+			for(index=0; index < arguments.length; ++index){
+				unsafeArg = arguments[index];
+				if(typeof(unsafeArg)==="function"){
+					safeArguments.push(SafeProxy.safe(unsafeArg))
+				}
+				else{
+					safeArguments.push(unsafeArg);
+				}
+			};
+		}
+		funcToProxy.apply(undefined, safeArguments);
+	};
+	return result;
+}
--- tests/qUnitTests.js
+++ tests/qUnitTests.js
@@ -28,14 +28,7 @@ functionWith5Args.initTestSupportValues = function(){
 	functionWith5Args.lastCallHadAllArguments = false;	
 }
 
-QUnit.test("Code continues executing after safe call to a failing function", function(assert){
-	var safeFunction = SafeProxy.safe(failingFunction);
-	badResult = safeFunction("some input");
-	assert.ok(1=="1","Passed!");
-});
-
-
-QUnit.test("Without SafeProxy, code fails to continue after calling a failing function", function(assert){
+QUnit.test("Without SafeProxy, code stops executing after calling a failing function", function(assert){
 	try{
 		var lastLineExecuted = false;
 		badResult = failingFunction("some input");
@@ -46,7 +39,14 @@ QUnit.test("Without SafeProxy, code fails to continue after calling a failing fu
 	assert.ok(!lastLineExecuted);
 });
 
-QUnit.test("SafeProxy passes all arguments to the original function", function(assert){
+QUnit.test("With SafeProxy, code continues executing after calling a failing function", function(assert){
+	var safeFunction = SafeProxy.safe(failingFunction);
+	badResult = safeFunction("some input");
+	assert.ok(1=="1","Passed!");
+});
+
+
+QUnit.test("Safe function passes all arguments to the original function", function(assert){
 	functionWith5Args.initTestSupportValues()
 	var safe5 = SafeProxy.safe(functionWith5Args);
 	safe5('1','2',3,'4',5);
@@ -54,7 +54,7 @@ QUnit.test("SafeProxy passes all arguments to the original function", function(a
 	assert.ok(functionWith5Args.lastCallHadAllArguments);
 });
 
-QUnit.test("SafeProxy does not copy any properties or methods of the original function", function(assert){
+QUnit.test("Safe function does not copy any properties or methods of the original function", function(assert){
 	functionWith5Args.initTestSupportValues()
 	var safe5 = SafeProxy.safe(functionWith5Args);
 	assert.ok(functionWith5Args.callCount!=undefined);
@@ -74,6 +74,26 @@ QUnit.test("Safe call to a failing function will raise the original exception", 
 	setTimeout(testForException,5);
 	//we cannot make a useful qUnit assert here. By design, the safe proxy will let qUnit finish before re-raising the exception.
 	assert.ok(true, "this test will throw an exception outside of the normal qUnit structure if it fails.");
+});
+
+QUnit.test("SafeProxy.safeParameters replaces each function in a method's parameters with a safe version of the same function", function(assert){
+	//ARRANGE:
+	originalMethod = this.spy();
+	var func1 = function(){};
+	var func2 = function(){};
+	var stubbedSafeFunction1 = function(){};
+	var stubbedSafeFunction2 = function(){};
+	this.stub(SafeProxy, "safe")
+	SafeProxy.safe.withArgs(func1).returns(stubbedSafeFunction1);
+	SafeProxy.safe.withArgs(func2).returns(stubbedSafeFunction2);
+	//ACT:
+	methodWithSafeParams = SafeProxy.safeParameters(originalMethod);
+	methodWithSafeParams("a",func1, func2);
+	//ASSERT:
+	args = originalMethod.args[0];
+	assert.equal(args[0],"a","Calls the original method with the same value for each non-function parameter");
+	assert.equal(args[1], stubbedSafeFunction1,"Calls the original method with a safe version of each function parameter");
+	assert.equal(args[2], stubbedSafeFunction2,"Calls the original method with a safe version of each function parameter");
 });
 
 
